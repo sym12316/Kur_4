112846dcfe5927ee44ca183f998fc2f4c99785ea
sym12316 <sym12316@gmail.com>
2023-11-21 23:12:44 +0900

    com3

--- main.py
+++ main.py
@@ -18,8 +18,50 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)  
         # self.showFullScreen()
-        self.label_date.setText(datetime.datetime.today().strftime('%d.%m.%Y')) 
-        print(self.label_date.text())
+
+
+        # self.label_date.setText(datetime.datetime.today().strftime('%d.%m.%Y')) 
+        self.label_date.setText('17.11.2023')
+
+        self.tableFil()
+    
+        self.pushButton_left.clicked.connect(self.onLeftBut)
+        self.pushButton_right.clicked.connect(self.onRightBut)
+
+    def onLeftBut(self):
+        ourDate = self.label_date.text()
+        Date2 = datetime.datetime.strptime(ourDate, '%d.%m.%Y') + datetime.timedelta(days=-1)
+        ourDate = Date2.strftime('%d.%m.%Y') 
+        self.label_date.setText(ourDate)
+        self.tableClean()
+        self.tableFil()
+
+
+    def onRightBut(self):
+        ourDate = self.label_date.text()
+        Date2 = datetime.datetime.strptime(ourDate, '%d.%m.%Y') + datetime.timedelta(days=1)
+        ourDate = Date2.strftime('%d.%m.%Y') 
+        self.label_date.setText(ourDate)
+        self.tableClean()
+        self.tableFil()
+        
+
+
+    def tableClean(self):
+        self.tableWidget.clear()
+
+        # for c in range(self.tableWidget.columnCount()):
+        #     it = self.tableWidget.item(r, c)
+        #     if it is not None:
+        #         it.setText("")
+        #     w = self.tableWidget.cellWidget(r, c)
+        #     if isinstance(w, QtGui.QLabel):
+        #         w.clear()
+
+    def tableFil(self):
+        print("АХАХАХАХАХХ РОМ")
+        ourDate = self.label_date.text()
+        # tableWidget.clear()
         # ↓ выбираем тип врача и ФИО
         cur.execute("SELECT Spec FROM 'Doc' ORDER BY ID_Doc ASC;")
         medSpec = cur.fetchall()
@@ -30,6 +72,11 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         # ↓ 
         # ↑ 
 
+
+        
+
+
+        
         # ↓ забиваем ячейки
         for i in range(len(docFIO)):
         
@@ -45,8 +92,7 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
             
             
             # ↓ берём нашу дату
-            ourDate = self.label_date.text()
-            ourDate = '15.11.2023'
+            
             # ↓ забиваем время
             time = ['9:00','9:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30']
             
@@ -62,40 +108,47 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
                 AND Day_appointment = (?));""",(time[j],docFIOStr,ourDate,))
                  # ↓ переформотируем tuple в int/str
                 currentAppointmentId = cur.fetchall()
-                currentAppointmentIdStr= int(''.join(map(str,currentAppointmentId[0])))
-                
-                 # ↓ запрос на выдачу ФИО по ID приёмов
-                cur.execute("""SELECT Pat_FIO FROM Patient_card
-                WHERE (ID_Pat = (SELECT ID_Pat FROM Appointment
-                WHERE ID_appointment = (?)))""",(currentAppointmentIdStr,))
-                patFio = cur.fetchall()
-                # ↓ переформотируем tuple в int/str
-                patFioStr = ''.join(patFio[0])
-                
-                 # ↓ Вставляем ФИО пациента в item
-                item.setData(Qt.EditRole, patFioStr)
+                if len(currentAppointmentId) > 0:
+                    currentAppointmentIdStr= int(''.join(map(str,currentAppointmentId[0])))
+                    
+                    # ↓ запрос на выдачу ФИО по ID приёмов
+                    cur.execute("""SELECT Pat_FIO FROM Patient_card
+                    WHERE (ID_Pat = (SELECT ID_Pat FROM Appointment
+                    WHERE ID_appointment = (?)))""",(currentAppointmentIdStr,))
+                    patFio = cur.fetchall()
+                    # ↓ переформотируем tuple в int/str
+                    patFioStr = ''.join(patFio[0])
+                    
+                    # ↓ Вставляем ФИО пациента в item
+                    item.setData(Qt.EditRole, patFioStr)
 
-                 # ↓ запрос на выдачу статуса записи по ID приёмов
-                cur.execute("""SELECT appointment_status FROM Appointment                      
-                WHERE ID_appointment = (?)""",(currentAppointmentIdStr,))
-                appointmentStatus = cur.fetchall()
-                # ↓ переформотируем tuple в int/str
-                appointmentStatusStr = int(''.join(map(str,appointmentStatus[0])))
-                print (appointmentStatusStr)
+                    # ↓ запрос на выдачу статуса записи по ID приёмов
+                    cur.execute("""SELECT appointment_status FROM Appointment                      
+                    WHERE ID_appointment = (?)""",(currentAppointmentIdStr,))
+                    appointmentStatus = cur.fetchall()
+                    # ↓ переформотируем tuple в int/str
+                    appointmentStatusStr = int(''.join(map(str,appointmentStatus[0])))
 
-                if appointmentStatusStr == 0:
-                    item.setBackground(QtGui.QColor(255, 128, 128))
-                elif  appointmentStatusStr == 1:
-                    item.setBackground(QtGui.QColor(200, 200, 200))
-                elif  appointmentStatusStr == 2:
-                    item.setBackground(QtGui.QColor(98, 111, 222))
-                print (j )
-                self.tableWidget.setItem(j+2, i, item)
-
-                # item.setBackground(QtGui.QColor(255, 128, 128))
-                # item.setBackground(QtGui.QColor(200, 200, 200))
-
-
+                    # ↓ проверка состояния записи, окрашивание ячейки
+                    if appointmentStatusStr == 0:
+                        item.setBackground(QtGui.QColor(255, 128, 128))
+                    elif  appointmentStatusStr == 1:
+                        item.setBackground(QtGui.QColor(200, 200, 200))
+                    elif  appointmentStatusStr == 2:
+                        item.setBackground(QtGui.QColor(98, 111, 222))
+                    
+                    # ↓ Вставляем данные в таблицу
+                    self.tableWidget.setItem(j+2, i, item)
+            self.tableWidget.itemClicked.connect(lambda: print('Button clicked'))
+    #     self.pushButton_right.clicked.connect(self.def1)
+    
+    # def def1(self):
+    #     self.label_date.setText('a')
+    # def def1(self):
+    #     self.appointmentDoctorWindow= appointmentDoctorWindow()
+    #     self.appointmentDoctorWindow.show()
+    #     self.hide()
+        # self.def2
 
 
 
